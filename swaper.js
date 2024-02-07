042b1d670e0c2e6f7715551f241d73b32389ea75
sandeepkundekar11 <106948198+sandeepkundekar11@users.noreply.github.com>
2024-02-07 12:20:36 +0530

    updated the File structure

--- swaper.js
+++ swaper.js
@@ -29,31 +29,3 @@ var swiper = new Swiper(".mySwiper", {
     enabled: true,
   },
 });
-
-// let swiper2 = new Swiper(".solutionSwiper", {
-//   slidesPerView: 1,
-//   spaceBetween: 30,
-//   loop: true,
-//   autoplay: {
-//     delay: 2000,
-//     disableOnInteraction: false,
-//   },
-//   pagination: {
-//     el: ".swiper-pagination2",
-//     clickable: true,
-//   },
-//   breakpoints: {
-//     640: {
-//       slidesPerView: 1,
-//       spaceBetween: 2,
-//     },
-//     768: {
-//       slidesPerView: 4,
-//       spaceBetween: 5,
-//     },
-//     1024: {
-//       slidesPerView: 4,
-//       spaceBetween: 8,
-//     },
-//   },
-// });
